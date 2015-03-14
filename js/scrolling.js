0ba87119b90d43ea7ef8e27f731708c0f82a8dea
Cory Mortimer <cory.m.mortimer@gmail.com>
2015-03-14 12:48:05 -0700

    Quick fix to disable animation on mobile

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,36 +1,40 @@
-var controller = new ScrollMagic.Controller();
+if ( $(window).width() > 768) { 
+    
+    var controller = new ScrollMagic.Controller();
 
-var fade_in_me = TweenMax.from("#aboutMe", 1, {opacity: 0, marginTop:"500px"});
-var fade_in_projects = TweenMax.from("#projects", 1, {opacity: 0, marginTop:"500px"});
-var fade_in_short_films = TweenMax.from("#shortFilms", 1, {opacity: 0, marginTop:"500px"});
-var fade_in_connect = TweenMax.from("#connect", 1, {opacity: 0, marginTop:"500px"});
+    var fade_in_me = TweenMax.from("#aboutMe", 1, {opacity: 0, marginTop:"500px"});
+    var fade_in_projects = TweenMax.from("#projects", 1, {opacity: 0, marginTop:"500px"});
+    var fade_in_short_films = TweenMax.from("#shortFilms", 1, {opacity: 0, marginTop:"500px"});
+    var fade_in_connect = TweenMax.from("#connect", 1, {opacity: 0, marginTop:"500px"});
 
-new ScrollMagic.Scene({
-    triggerElement: '#aboutMeTrigger',
-    reverse:false
-})
-.setTween(fade_in_me)
-.addTo(controller);
+    new ScrollMagic.Scene({
+        triggerElement: '#aboutMeTrigger',
+        reverse:false
+    })
+    .setTween(fade_in_me)
+    .addTo(controller);
 
 
 
-new ScrollMagic.Scene({
-    triggerElement: '#projectsTrigger',
-    reverse:false
-})
-.setTween(fade_in_projects)
-.addTo(controller);
+    new ScrollMagic.Scene({
+        triggerElement: '#projectsTrigger',
+        reverse:false
+    })
+    .setTween(fade_in_projects)
+    .addTo(controller);
 
-new ScrollMagic.Scene({
-    triggerElement: '#shortFilmsTrigger',
-    reverse:false
-})
-.setTween(fade_in_short_films)
-.addTo(controller);
+    new ScrollMagic.Scene({
+        triggerElement: '#shortFilmsTrigger',
+        reverse:false
+    })
+    .setTween(fade_in_short_films)
+    .addTo(controller);
 
-new ScrollMagic.Scene({
-    triggerElement: '#connectTrigger',
-    reverse:false
-})
-.setTween(fade_in_connect)
-.addTo(controller);
+    new ScrollMagic.Scene({
+        triggerElement: '#connectTrigger',
+        reverse:false
+    })
+    .setTween(fade_in_connect)
+    .addTo(controller);
+
+}
